a78d1d0cb685dca72bf69e56f2fb0351d8d7ca67
ishwar2303 <68193554+ishwar2303@users.noreply.github.com>
2021-01-26 07:09:28 +0530

    updated

--- connection.php
+++ connection.php
@@ -5,11 +5,6 @@ $user_name = 'root';
 $password = '';
 $db_name = 'bank';
 
-// $server_name = 'database-1.cnoowobui7gh.ap-south-1.rds.amazonaws.com';
-// $user_name = 'ishwar';
-// $password = 'qwerty1234';
-// $db_name = 'bank';
-
 $conn = new mysqli($server_name, $user_name, $password, $db_name);
 
 if($conn->connect_error)
