26dcfba024abb835ed02ed68fa3467b7f3b7457f
ishwar2303 <68193554+ishwar2303@users.noreply.github.com>
2021-02-09 23:42:38 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,10 +9,5 @@ $db_name = 'bank';
 $conn = new mysqli($server_name, $user_name, $password, $db_name);
 
 if($conn->connect_error)
-<<<<<<< HEAD
     die(header('Location: pages/samples/error-500.html'));
 ?>
-=======
-    die(header('Location: 505-error.php'));
-?>
->>>>>>> 298f8df5082f1993ca81dca155b25bfd8f46c72e
