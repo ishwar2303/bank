edfec0465e0f1448d0d1e64e91b47e202a138228
ishwar2303 <68193554+ishwar2303@users.noreply.github.com>
2021-02-11 11:37:46 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,17 +5,8 @@ $user_name = 'root';
 $password = '';
 $db_name = 'bank';
 
-// $server_name = 'database-1.c3mppahrvey4.ap-south-1.rds.amazonaws.com';
-// $user_name = 'ishwar';
-// $password = 'qwerty1234';
-// $db_name = 'bank';
-
 $conn = new mysqli($server_name, $user_name, $password, $db_name);
 
 if($conn->connect_error)
     die(header('Location: pages/samples/error-500.html'));
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> ef88602d97f118a7eb49ab1991454bdc474f5ea2
